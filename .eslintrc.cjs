81bfb2603984272dff8f78d798abf24aafbb5856
Onofrei Costinel Danut <onofreicostineldanut12@gmail.com>
2025-03-07 17:18:41 +0200

    chore: updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,148 +1,162 @@
 module.exports = {
     root: true,
     ignorePatterns: [],
-    env: {
-        node: true,
-        browser: true,
-    },
-    parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module',
-    },
-    reportUnusedDisableDirectives: false,
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'standard',
-        'prettier',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/stylistic',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'plugin:@eslint-community/eslint-comments/recommended',
-    ],
-    plugins: ['unicorn'],
-    settings: {
-        'import/resolver': {
-            typescript: {
-                alwaysTryTypes: true,
-                project: './tsconfig.json',
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.mjs', '*.cjs'],
+            env: {
+                node: true,
+                browser: true,
             },
-        },
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-    },
-    rules: {
-        'no-lonely-if': 'error',
-        'no-warning-comments': 'off',
-        'no-implicit-coercion': [
-            'error',
-            {
-                allow: ['!!'],
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
             },
-        ],
-        'array-callback-return': [
-            'error',
-            {
-                allowVoid: true,
-                checkForEach: true,
-                allowImplicit: true,
+            parser: '@typescript-eslint/parser',
+            reportUnusedDisableDirectives: false,
+            extends: [
+                'standard',
+                'prettier',
+                'eslint:recommended',
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/stylistic',
+                'plugin:import/recommended',
+                'plugin:import/typescript',
+                'plugin:@eslint-community/eslint-comments/recommended',
+            ],
+            plugins: ['unicorn'],
+            settings: {
+                'import/resolver': {
+                    typescript: true,
+                    node: true,
+                },
+                'import/parsers': {
+                    '@typescript-eslint/parser': ['.ts', '.tsx'],
+                },
             },
-        ],
-
-        'unicorn/prefer-prototype-methods': 'error',
-        'unicorn/prefer-default-parameters': 'error',
-        'unicorn/prefer-optional-catch-binding': 'error',
-        'unicorn/prefer-native-coercion-functions': 'error',
-        'unicorn/prefer-logical-operator-over-ternary': 'error',
-
-        'unicorn/escape-case': 'error',
-        'unicorn/no-lonely-if': 'error',
-        'unicorn/error-message': 'error',
-        'unicorn/no-useless-spread': 'error',
-        'unicorn/no-zero-fractions': 'error',
-        'unicorn/number-literal-case': 'error',
-        'unicorn/no-useless-undefined': 'error',
-        'unicorn/no-unnecessary-await': 'error',
-        'unicorn/expiring-todo-comments': [
-            'error',
-            {
-                allowWarningComments: false,
-            },
-        ],
-        'unicorn/no-length-as-slice-end': 'error',
-        'unicorn/consistent-function-scoping': 'error',
-        'unicorn/no-await-in-promise-methods': 'error',
-        'unicorn/no-useless-fallback-in-spread': 'error',
-        'unicorn/consistent-empty-array-spread': 'error',
-        'unicorn/no-invalid-remove-event-listener': 'error',
-        'unicorn/no-useless-promise-resolve-reject': 'error',
-        'unicorn/no-single-promise-in-promise-methods': 'error',
-
-        yoda: [
-            'error',
-            'always',
-            {
-                onlyEquality: true,
-            },
-        ],
-
-        'no-shadow': 'off',
-        '@typescript-eslint/no-shadow': 'error',
-
-        camelcase: 'off',
-        '@typescript-eslint/naming-convention': [
-            'error',
-            { format: null, selector: 'objectLiteralProperty' },
-            {
-                selector: 'default',
-                leadingUnderscore: 'allowSingleOrDouble',
-                trailingUnderscore: 'allowSingleOrDouble',
-                format: ['snake_case', 'camelCase', 'PascalCase', 'UPPER_CASE'],
-            },
-        ],
-
-        '@eslint-community/eslint-comments/disable-enable-pair': 'off',
-        '@eslint-community/eslint-comments/no-unused-disable': 'error',
-
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            {
-                args: 'all',
-                caughtErrors: 'all',
-                argsIgnorePattern: '^_',
-                varsIgnorePattern: '^_',
-                ignoreRestSiblings: true,
-                caughtErrorsIgnorePattern: '^_',
-                destructuredArrayIgnorePattern: '^_',
-            },
-        ],
-
-        'import/order': [
-            'error',
-            {
-                distinctGroup: true,
-                'newlines-between': 'always',
-                warnOnUnassignedImports: true,
-                pathGroupsExcludedImportTypes: ['type'],
-                pathGroups: [
+            rules: {
+                'no-lonely-if': 'error',
+                'no-warning-comments': 'off',
+                'no-implicit-coercion': [
+                    'error',
                     {
-                        group: 'unknown',
-                        position: 'after',
-                        pattern: '@/app/**',
+                        allow: ['!!'],
                     },
                 ],
-                alphabetize: {
-                    order: 'asc',
-                    orderImportKind: 'asc',
-                    caseInsensitive: true,
-                },
-                groups: ['type', 'builtin', 'external', 'internal', 'unknown', 'parent', 'sibling', 'index', 'object'],
+                'array-callback-return': [
+                    'error',
+                    {
+                        allowVoid: true,
+                        checkForEach: true,
+                        allowImplicit: true,
+                    },
+                ],
+
+                'unicorn/prefer-prototype-methods': 'error',
+                'unicorn/prefer-default-parameters': 'error',
+                'unicorn/prefer-optional-catch-binding': 'error',
+                'unicorn/prefer-native-coercion-functions': 'error',
+                'unicorn/prefer-logical-operator-over-ternary': 'error',
+
+                'unicorn/escape-case': 'error',
+                'unicorn/no-lonely-if': 'error',
+                'unicorn/error-message': 'error',
+                'unicorn/no-useless-spread': 'error',
+                'unicorn/no-zero-fractions': 'error',
+                'unicorn/number-literal-case': 'error',
+                'unicorn/no-useless-undefined': 'error',
+                'unicorn/no-unnecessary-await': 'error',
+                'unicorn/expiring-todo-comments': [
+                    'error',
+                    {
+                        allowWarningComments: true,
+                    },
+                ],
+                'unicorn/no-length-as-slice-end': 'error',
+                'unicorn/consistent-function-scoping': 'error',
+                'unicorn/no-await-in-promise-methods': 'error',
+                'unicorn/no-useless-fallback-in-spread': 'error',
+                'unicorn/consistent-empty-array-spread': 'error',
+                'unicorn/no-invalid-remove-event-listener': 'error',
+                'unicorn/no-useless-promise-resolve-reject': 'error',
+                'unicorn/no-single-promise-in-promise-methods': 'error',
+
+                yoda: [
+                    'error',
+                    'always',
+                    {
+                        onlyEquality: true,
+                    },
+                ],
+
+                'no-shadow': 'off',
+                '@typescript-eslint/no-shadow': 'error',
+
+                camelcase: 'off',
+                '@typescript-eslint/naming-convention': [
+                    'error',
+                    { format: null, selector: 'typeMethod' },
+                    { format: null, selector: 'typeProperty' },
+                    { format: null, selector: 'objectLiteralMethod' },
+                    { format: null, selector: 'objectLiteralProperty' },
+                    {
+                        selector: 'default',
+                        leadingUnderscore: 'allowSingleOrDouble',
+                        trailingUnderscore: 'allowSingleOrDouble',
+                        format: ['snake_case', 'camelCase', 'PascalCase', 'UPPER_CASE'],
+                    },
+                ],
+
+                '@eslint-community/eslint-comments/disable-enable-pair': 'off',
+                '@eslint-community/eslint-comments/no-unused-disable': 'error',
+
+                '@typescript-eslint/no-unused-vars': [
+                    'error',
+                    {
+                        args: 'all',
+                        caughtErrors: 'all',
+                        argsIgnorePattern: '^_',
+                        varsIgnorePattern: '^_',
+                        ignoreRestSiblings: true,
+                        caughtErrorsIgnorePattern: '^_',
+                        destructuredArrayIgnorePattern: '^_',
+                    },
+                ],
+
+                'import/order': [
+                    'error',
+                    {
+                        distinctGroup: true,
+                        'newlines-between': 'always',
+                        warnOnUnassignedImports: true,
+                        pathGroupsExcludedImportTypes: ['type'],
+                        pathGroups: [
+                            {
+                                group: 'unknown',
+                                position: 'after',
+                                pattern: '@/app/**',
+                            },
+                        ],
+                        alphabetize: {
+                            order: 'asc',
+                            orderImportKind: 'asc',
+                            caseInsensitive: true,
+                        },
+                        groups: [
+                            'type',
+                            'builtin',
+                            'external',
+                            'internal',
+                            'unknown',
+                            'parent',
+                            'sibling',
+                            'index',
+                            'object',
+                        ],
+                    },
+                ],
             },
-        ],
-    },
-    overrides: [
+        },
         {
             plugins: ['sort-exports'],
             files: ['**/index.js', '**/index.ts'],
